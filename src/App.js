1c9286ebf9476afb749f520f743d4dff465291ee
Pratik Sharma <prtis@amazon.com>
2020-11-18 19:23:44 +0530

    Lesson 14: Outputting lists

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import { Component } from 'react';
 import Prtis from './prtis'
 
+/*
 function App() {
   return (
     <div className="App">
@@ -8,6 +10,24 @@ function App() {
       <Prtis name="Krishna" alias="uppalak"/>
     </div>
   );
+}
+*/
+class App extends Component {
+  state = {
+    prtis : [
+      { name: 'Pratik', alias: 'prtis@' },
+      { name: 'Sai', alias: 'rathsai@' }
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hey! Wassup!?</h1>
+        <Prtis prtis={this.state.prtis}/>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/prtis.js
+++ src/prtis.js
@@ -4,12 +4,20 @@ class Prtis extends Component {
     render() {
         // Assigns the variabled from props to 
         // the consts of the same name which can then be referenced directly
-        const { name, alias } = this.props;
+        const { prtis } = this.props;
+        const prtis_list = prtis.map(prtis => {
+            return (
+                <div className="prtis" key={prtis.alias} >
+                    <p>The prtis@ component</p>
+                    <p>My name is: { prtis.name }</p>
+                    <p>My alias is: { prtis.alias } </p>
+                </div>
+            )
+        })
+        // Use the map function to map this array to a new template array
         return(
-            <div classname="prtis">
-                <p>The prtis@ component</p>
-                <p>My name is: {name}</p>
-                <p>My alias is: {alias}</p>
+            <div className='ninja-list'>
+                { prtis_list }
             </div>
         )
     }

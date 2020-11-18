1d8b539d6c3dd7c1952e201131237f5cf6e7f2cd
Pratik Sharma <prtis@amazon.com>
2020-11-18 18:50:37 +0530

    Lesson 13: Props

---
+++ src/prtis.js
@@ -0,0 +1,13 @@
+import React,  { Component } from 'react';
+
+class Prtis extends Component {
+    render() {
+        return(
+            <div classname="prtis">
+                <p>The prtis@ component</p>
+            </div>
+        )
+    }
+}
+
+export default Prtis

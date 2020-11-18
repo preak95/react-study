e148e7038ae347e12839fa5be1061ef0af98db2f
Pratik Sharma <prtis@amazon.com>
2020-11-18 23:31:19 +0530

    Lesson 16: Conditional outout, in component.

--- src/prtis.js
+++ src/prtis.js
@@ -5,15 +5,24 @@ const Prtis = ({prtis}) => {
     // Assigns the variabled from props to 
     // the consts of the same name which can then be referenced directly
     const prtis_list = prtis.map(prtis => {
-        return (
-            <div className="prtis" key={prtis.alias} >
-                <p>The prtis@ component</p>
-                <p>My name is: { prtis.name }</p>
-                <p>My alias is: { prtis.alias } </p>
-            </div>
-        )
+        // Another way to use conditions
+        // condition ? (if_true) : (if_false)
+        // prtis.alias === 'prtis@' ? <blah_blah> ? null
+        if (prtis.alias === 'prtis@') {
+            return (
+                <div className="prtis" key={prtis.alias} >
+                    <p>The prtis@ component</p>
+                    <p>My name is: { prtis.name }</p>
+                    <p>My alias is: { prtis.alias } </p>
+                </div>
+            )
+        } 
+        else{
+            return null
+        }
     })
     // Use the map function to map this array to a new template array
+    console.log(prtis_list);
     return(
         <div className='ninja-list'>
             { prtis_list }

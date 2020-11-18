cdcee64f7619545eb12c68c2a97823305112c498
Pratik Sharma <prtis@amazon.com>
2020-11-18 23:20:11 +0530

    Lesson 15: Stateless components. For UI componenets you don't need classes and they can just be created using functions. And props are passed as an argument

--- src/prtis.js
+++ src/prtis.js
@@ -1,26 +1,24 @@
 import React,  { Component } from 'react';
 
-class Prtis extends Component {
-    render() {
-        // Assigns the variabled from props to 
-        // the consts of the same name which can then be referenced directly
-        const { prtis } = this.props;
-        const prtis_list = prtis.map(prtis => {
-            return (
-                <div className="prtis" key={prtis.alias} >
-                    <p>The prtis@ component</p>
-                    <p>My name is: { prtis.name }</p>
-                    <p>My alias is: { prtis.alias } </p>
-                </div>
-            )
-        })
-        // Use the map function to map this array to a new template array
-        return(
-            <div className='ninja-list'>
-                { prtis_list }
+// Deconsutructing props directly in the function argument
+const Prtis = ({prtis}) => {
+    // Assigns the variabled from props to 
+    // the consts of the same name which can then be referenced directly
+    const prtis_list = prtis.map(prtis => {
+        return (
+            <div className="prtis" key={prtis.alias} >
+                <p>The prtis@ component</p>
+                <p>My name is: { prtis.name }</p>
+                <p>My alias is: { prtis.alias } </p>
             </div>
         )
-    }
+    })
+    // Use the map function to map this array to a new template array
+    return(
+        <div className='ninja-list'>
+            { prtis_list }
+        </div>
+    )
 }
 
 export default Prtis
